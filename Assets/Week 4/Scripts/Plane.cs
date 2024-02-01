9457b4a9397f9de0f531ca376c9c39191082d218
Parshaun <Parshaun@OA-S141-21>
2024-02-01 15:16:33 -0500

    Week 4 Task 2 Complete. Plane Moving.

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -8,17 +8,61 @@ public class Plane : MonoBehaviour
     public List<Vector2> points;
     public float newPointThreshold = 0.2f;
     Vector2 lastPosition;
+    Vector2 currentPosition;
     LineRenderer lineRenderer;
+    public Rigidbody2D rigidbody;
+    public float speed = 2f;
+    public AnimationCurve landing;
+    float timerValue;
 
     private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0, transform.position);
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    private void FixedUpdate()
+    {
+        currentPosition = transform.position;
+        if (points.Count > 0)
+        {
+            Vector2 direction = points[0] - currentPosition;
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            rigidbody.rotation = -angle;
+        }
+        rigidbody.MovePosition(rigidbody.position + (Vector2)transform.up * speed * Time.deltaTime);
+    }
 
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            timerValue += 0.5f * Time.deltaTime;
+            float interpolation = landing.Evaluate(timerValue);
+            if (transform.localScale.z < 0.1f)
+            {
+                Destroy(gameObject);
+            }
+            transform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, interpolation);
+        }
 
+        lineRenderer.SetPosition(0, transform.position);
+        if (points.Count > 0)
+        {
+            if (Vector2.Distance(currentPosition, points[0]) < newPointThreshold)
+            {
+                points.RemoveAt(0);
+
+                for (int i = 0; i < lineRenderer.positionCount - 2; i++)
+                {
+                    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i + 1));
+                }
+                lineRenderer.positionCount--;
+            }
+        }
+    }
     private void OnMouseDown()
     {
         points = new List<Vector2>();
