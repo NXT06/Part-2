3ef2c9b5c12df2f2ab52f91d3c2ff614904a13bf
Shaun Parker <parshaun@shernet.sheridancollege.ca>
2024-02-15 17:31:36 -0500

    Task 4a Complete

---
+++ Assets/Week 6/Scripts/ScreenResolution.cs
@@ -0,0 +1,25 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenResolution : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    public void SetWidth(int newWidth)
+    {
+        width = newWidth;
+
+    }
+
+    public void SetHeight(int newHeight)
+    {
+        height = newHeight;
+    }
+    public void SetRes() {
+        Screen.SetResolution(width, height, false);
+    }
+}
